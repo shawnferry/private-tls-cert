9462e3bb7c21d84c63456e2908a99abab3f8943f
Shawn Ferry <shawn.ferry@genpactadconn.onmicrosoft.com>
2020-03-04 13:57:20 -0500

    expose certs

--- modules/generate-cert/outputs.tf
+++ modules/generate-cert/outputs.tf
@@ -1,12 +1,25 @@
 output "ca_public_key_file_path" {
-  value = "${var.ca_public_key_file_path}"
+  value = var.ca_public_key_file_path
 }
 
 output "public_key_file_path" {
-  value = "${var.public_key_file_path}"
+  value = var.public_key_file_path
 }
 
 output "private_key_file_path" {
-  value = "${var.private_key_file_path}"
+  value = var.private_key_file_path
 }
 
+output "ca_priv_key" {
+  value = tls_private_key.ca
+}
+output "ca_cert" {
+  value = tls_self_signed_cert.ca
+}
+
+output "cert_priv_key" {
+  value = tls_private_key.cert
+}
+output "cert_local" {
+  value = tls_locally_signed_cert.cert
+}
